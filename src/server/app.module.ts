a6f59e402ec75c5ddb38e5339c96d490deeddacf
Lívia <liviapnd@gmail.com>
2024-02-16 20:01:07 -0300

    chore: adicionando base inicial nest-next

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -3,15 +3,13 @@ import { RenderModule } from 'nest-next';
 import Next from 'next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PetsModule } from './pets/pets.module';
 
 @Module({
   imports: [
     RenderModule.forRootAsync(
       Next({ dev: true }),
       { viewsDir: null }
-    ),
-    PetsModule
+    )
   ],
   controllers: [AppController],
   providers: [AppService],
